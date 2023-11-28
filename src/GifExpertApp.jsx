3f195a0878b869326499097ea0d2014866eb3b09
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-11-28 07:49:58 -0500

    removing button

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,21 +5,12 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['Fifa 2023', 'SpiderMan']);
   
-  const onAddCategory = () => {
-    
-    setCategories([ ...categories, ['Fifa 2024']]);
-    
-  }
-
-
-
   return (
     <>
-      <h1>GifExpertApp</h1>
 
       <div className='center-form'>
-        <AddCategory/>
-        <button onClick={onAddCategory}>New category</button>
+      <h1>GifsExpertApp..</h1> 
+        <AddCategory setCategories={setCategories}/>
       </div>
 
       
