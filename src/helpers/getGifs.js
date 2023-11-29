2b08f912f5d72cfb25034bce101e961789b460a4
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-11-29 06:27:50 -0500

    getgif

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+
+export const getGifs = async(category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=Op5KzFfttVVPD0lzAIo2YnfBuwNPAsyo&q=${category}&limit=10`;
+  const response = await fetch( url );
+  const { data = [] } = await response.json();
+
+  const gifs = data.map( images => ({
+    id: images.id,
+    title: images.title,
+    url: images.images.downsized_medium.url
+  }))
+
+  console.log(gifs);
+  return gifs;
+}
