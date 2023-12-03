2a1aa88ecc3a6b94e62af4dfcf8768b1c873da37
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-12-02 19:37:47 -0500

    testing pending..

---
+++ test/GifExpertApp.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { GifExpertApp } from "../src/GifExpertApp";
+
+
+describe('Testing GifExpertApp', () => {
+
+  test('should first', () => {p
+
+    render( <GifExpertApp/> );
+  
+    screen.debug();
+
+  })
+
+
+});
