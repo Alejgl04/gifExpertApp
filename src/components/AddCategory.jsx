1cc79a2db962234d14a78875c4d859351d90dc04
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-12-02 19:03:55 -0500

    Testing on addCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
 
 export const AddCategory = ({ onNewCategory }) => {
 
@@ -14,13 +16,11 @@ export const AddCategory = ({ onNewCategory }) => {
     if ( inputValue.trim().length <= 1 ) return;
     setInputValue('');
     onNewCategory( inputValue.trim() );
-
-    // setCategories( categories => [ inputValue, ...categories])
   }
 
   return (
 
-    <form onSubmit={ onSubmit }>
+    <form aria-label='form' onSubmit={ onSubmit }>
       <input 
         type="text"
         placeholder="Search gifs"
@@ -30,4 +30,8 @@ export const AddCategory = ({ onNewCategory }) => {
     </form>
 
   )
+}
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
 }
---
+++ test/components/AddCategory.test.jsx
@@ -0,0 +1,51 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { AddCategory } from '../../src/components/AddCategory';
+
+describe('Testing on AddCategory', () => {
+
+  const inputValue = 'Spiderman';
+
+  test('Should change the value the input text', () => {
+
+    render( <AddCategory onNewCategory={ () => {} }/> );
+
+    const input = screen.getByRole('textbox');
+
+    fireEvent.input( input, { target: { value: inputValue } } );
+
+    expect( input.value ).toBe('Spiderman');
+
+  });
+
+  test('should call onNewCategory if the input has a value', () => {
+
+    const onNewCategory = jest.fn();
+
+    render( <AddCategory onNewCategory={ onNewCategory }/> );
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input( input, { target: { value: inputValue } } );
+    fireEvent.submit( form );
+    
+    expect( input.value ).toBe('');
+    expect( onNewCategory ).toHaveBeenCalled();
+    expect( onNewCategory ).toHaveBeenCalledTimes(1);
+    expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+
+  });
+
+  test('should not call onNewCategory if the input has not a value', () => {
+
+    const onNewCategory = jest.fn();
+    render( <AddCategory onNewCategory={ onNewCategory }/> );
+
+    const form = screen.getByRole('form');
+    fireEvent.submit( form );
+
+    expect( onNewCategory ).toHaveBeenCalledTimes(0);
+
+  });
+
+})
